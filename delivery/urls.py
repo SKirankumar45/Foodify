136ea84dadfbf41d10a27eb048c6479b70782a93
SKirankumar45 <kirankumar02427@gmail.com>
2025-11-06 23:18:32 +0530

    first commit

---
+++ delivery/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    path('open_signin/',views.open_singin, name='open_signin'),
+    path('open_signup/',views.open_signup, name='open_signup'),
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signin, name='signin'),
+    path('signin/open_add_restaurant/', views.open_add_restaurant, name='open_add_restaurant'),
+    path('add_restaurant/',views.add_restaurant, name="add_restaurant"),
+    path('add_restaurant/open_update_restaurant/<int:restaurant_id>',views.open_update_restaurant,name="open_update_restaurant"),
+    path('signin/open_show_restaurant/',views.open_show_restaurant,name="open_show_restaurant"),
+    path('update_restaurant/<int:restaurant_id>',views.update_restaurant,name="update_restaurant"),
+    path('delete_restaurant/<int:restaurant_id>',views.delete_restaurant,name="delete_restaurant"),
+    path('open_update_menu/<int:restaurant_id>',views.open_update_menu,name="open_update_menu"),
+    path('update_menu/<int:restaurant_id>',views.update_menu,name="update_menu"),
+    path('view_menu/<int:restaurant_id>/<str:username>',views.view_menu,name="view_menu")
+]
