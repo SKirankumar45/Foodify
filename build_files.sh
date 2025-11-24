2fe10f98bb305584b4fd8752f5e7c5c383618f5f
SKirankumar45 <kirankumar02427@gmail.com>
2025-11-24 15:54:44 +0530

    10

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
-pip install -r requirements.txt
-python3.11 manage.py collectstatic
+#!/bin/bash
+echo "Building the project..."
+
+# --- FIX: Install dependencies here ---
+python3 -m pip install -r requirements.txt
+
+# Now you can safely run Django commands
+echo "Make Migrations..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3 manage.py collectstatic --noinput --clear
